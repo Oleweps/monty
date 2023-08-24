dac1382b95f22f6162278df1e6232de621b950a0
oleweps <weps6179@gmail.com>
2023-08-24 10:30:32 +0000

    0x19. C - Stacks, Queues - LIFO, FIFO

--- add_monty.c
+++ add_monty.c
@@ -12,18 +12,22 @@
 
 stack_t *add(stack_t **stack, int line)
 {
-	int len, sum;
+	stack_t *top = *stack;
+	int sum = top->n + top->next->n;
+	stack_t *temp = top->next;
 
-	len = stack_length(*stack); /** get length of stack */
-	if (len < 2)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		line_or_integer_err(": can't add, stack too short", line);
 		exit(EXIT_FAILURE);
 	}
 
-	sum = (*stack)->n + (*stack)->next->n;
-	(*stack)->next->n = sum;
-	pop(stack, line);
+	top->next->n = sum;
+	top->next = temp->next;if (temp->next)
+	{
+		temp->next->prev = top;
+	}
+	free(temp);
 
-	return (*stack);
+	return (top);
 }
